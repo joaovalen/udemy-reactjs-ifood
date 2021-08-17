68dcf633c5fc129f70adebbf8bc71086a473dd43
joaovalen <joao.valentini@randon.com.br>
2021-08-17 14:16:35 -0300

    full project

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,21 +1,21 @@
 import React from 'react'
 
+import HeaderCartButton from './HeaderCartButton';
 import classes from './Header.module.css'
 import foodImage from '../../assets/food.jpg' 
 
 const Header = (props) => {
     return (
-        <>
-            <header className={classes.header}>
-                <h1>ReactFood</h1>
-                <button>Cart</button>
-            </header>
-            <div className={classes.mainImage}>
-                <img src={foodImage} alt="Prato de Comida"/>
-            </div>
-        </>
-    )
-
-};
-
-export default Header;
+      <>
+        <header className={classes.header}>
+          <h1>ReactMeals</h1>
+          <HeaderCartButton onClick={props.onShowCart} />
+        </header>
+        <div className={classes['main-image']}>
+          <img src={foodImage} alt='A table full of delicious food!' />
+        </div>
+      </>
+    );
+  };
+  
+  export default Header;
